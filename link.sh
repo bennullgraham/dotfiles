07d6871ae3142ef0c6a7cd771ca71f967e88f8bc
Ben Graham <bgraham@bgraham.com.au>
2013-08-21 20:52:30 +1000

    correct linkscript conditionals

--- link.sh
+++ link.sh
@@ -7,11 +7,11 @@ links="vimrc vim"
 
 # Before trying to make any links, do a sanity-check
 for link in $links; do
-    if [ -z "$src/$link" ]; then
+    if [ ! -e "$src/$link" ]; then
         echo "Source dotfile $src/$link doesn't seem to exist, bailing out."
         exit 1
     fi
-    if [ ! -z "$dest/.$link" -a ! -L "$dest/.$link" ]; then
+    if [ -e "$dest/.$link" -a ! -L "$dest/.$link" ]; then
         echo "Target dotfile $dest/.$link already exists, bailing out."
         exit 1
     fi
