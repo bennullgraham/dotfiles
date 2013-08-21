95382baa5147b1598dea270560b4eefda97cfe35
Ben Graham <bgraham@bgraham.com.au>
2013-08-21 20:45:48 +1000

    initial vim config and linker script

---
+++ link.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+src="$HOME/.dotfiles"
+dest="$HOME"
+
+links="vimrc vim"
+
+# Before trying to make any links, do a sanity-check
+for link in $links; do
+    if [ -z "$src/$link" ]; then
+        echo "Source dotfile $src/$link doesn't seem to exist, bailing out."
+        exit 1
+    fi
+    if [ ! -z "$dest/.$link" -a ! -L "$dest/.$link" ]; then
+        echo "Target dotfile $dest/.$link already exists, bailing out."
+        exit 1
+    fi
+done
+
+# Now, try to make some symlinks. Skip any existing links.
+for link in $links; do
+    if [ -L "$dest/.$link" ]; then
+        echo "Not changing $dest/.$link, it's already a symlink."
+    else
+        echo "Symlinking $src/$link to $dest/.$link"
+        ln -s "$src/$link" "$dest/.$link"
+    fi
+done
+
+
---
+++ vimrc
@@ -0,0 +1,9 @@
+set t_Co=256
+set bg=dark
+colorscheme inkpot
+syntax enable
+
+set smartindent
+set tabstop=4
+set shiftwidth=4
+set expandtab
